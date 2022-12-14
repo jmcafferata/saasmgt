b22a43ab51fe1b2c898c08d9836b799895a75f0c
JM <jmcafferata@gmail.com>
2022-12-14 20:57:22 -0300

    Testing

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ req.onload = function () {
             });
             updateSaaS(saasList.data)
             displayStore(saasList.data)
+            console.log(saasList.data)
         } else {
             console.log("Error: " + req.statusText);
         }
@@ -25,30 +26,66 @@ req.send(null);
 
 // Updates SaaS list with extra information and converts price/year
 function updateSaaS(saasList) {
+    saasList.sort((a, b) => {
+        if (a.name > b.name) return 1;
+        if (a.name < b.name) return -1;
+        return 0;
+    });
     saasList.forEach(saas => {
         saas.acquired = false
         saas.priceAnnual = saas.price * 11
     })
 }
 
+
 function displayStore(saasList) {
-    let availableGrid = document.getElementById("available-grid");
-    let notAvailableGrid = document.getElementById("not-available-grid");
+    let marketplaceGrid = document.getElementById("marketplace-grid");
     let mySaasGrid = document.getElementById("my-saas-grid");
+    let unavDivs = []
 
     saasList.forEach(saas => {
         let div = document.createElement("div");
         let img = document.createElement("img");
+        let name = document.createElement("p")
+        name.textContent = saas.name
+        name.classList.add("saas-name")
         img.src = 'img/logos/' + saas.logo;
         div.appendChild(img);
+        div.appendChild(name)
         if (saas.acquired == false && saas.price <= player.budget) {
-            availableGrid.appendChild(div);
-        } 
+            marketplaceGrid.appendChild(div);
+        }
         else if (saas.acquired == false && saas.price > player.budget) {
-            notAvailableGrid.appendChild(div);
-        } 
+            div.classList.add("unavailable")
+            unavDivs.push(div)
+        }
         else if (saas.acquired == true) {
             mySaasGrid.appendChild(div);
         }
     });
+
+    // Push unavailable divs at the end
+    unavDivs.forEach(div => {
+        marketplaceGrid.appendChild(div);
+    })
+
 }
+
+
+
+//Timer 
+
+let timer = 59;
+      let countDown = setInterval(function() {
+        if (timer === 0) {
+          clearInterval(countDown);
+          gameOver();
+        } else {
+          document.getElementById("timer").innerHTML = Math.floor(timer/60) + ':' + (timer%60);
+        }
+        timer--;
+      }, 1000);
+
+      function gameOver() {
+        alert("Game Over!");
+      }
